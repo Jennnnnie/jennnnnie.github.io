3da30879b2f61082bedb3fea519b55a02a658056
Jennie <104903826+Jennnnnie@users.noreply.github.com>
2023-10-15 16:12:29 -0400

    Merge Sort Assignment 2

---
+++ script2.js
@@ -0,0 +1,30 @@
+// Add another function called mergeSort
+
+// 1 is recursive
+// 2 divide & conquer (essentially splitting array into 2 equal parts)
+
+const arr = [7, 9, 3, 5, 15, 10, 0];
+
+const mergeSort = (arr) => {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+};
+
+const merge = (leftArr, rightArr) => {
+  const sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+};
+
+console.log(mergeSort(arr));
